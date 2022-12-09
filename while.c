b27202b3b5345516b5a4dbc100568fbc39bf891d
ted0326 <109950607+ted0326@users.noreply.github.com>
2022-12-09 16:43:55 +0900

    Create while.c

---
+++ while.c
@@ -0,0 +1,13 @@
+/**
+* shell.c:shell program
+* author:lee seo bin
+* email:sltenic@naver.com
+* version:1.0
+* date:2022.12.06
+**/
+#include<stdio.h>
+int main() {
+	while (1) {
+	}
+	return 0;
+}
